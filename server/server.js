d90c03295763d117e8562be88d95cb38b823068a
Eray Sahin <eraysahinmoon@gmail.com>
2024-03-13 09:58:49 -0400

    Added Docker

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.delete('/product/:id', async (req, res) => {
     }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log('Server running on PORT: ' + PORT)
 })
